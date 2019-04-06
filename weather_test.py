914580758911b44b1f9578bc701d672ab68e9d65
kimura-kuya <cooya.kimura@gmail.com>
2019-04-06 09:21:59 +0900

    Delete weather_test.py

--- weather_test.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-import pprint
-url = "http://weather.livedoor.com/forecast/webservice/json/v1?city={}"
-city_id = 130010
-json_url = url.format(city_id)
-r = requests.get(json_url)
-json = r.json()
-pprint.pprint(json)
